d4b2dc689fd9ec3fef64899dc5729c7fffa4d39a
nihao <neil@thecover.co>
2021-12-22 18:20:09 +0800

    sql

---
+++ db.sql
@@ -0,0 +1,24 @@
+CREATE TABLE `meta` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) NOT NULL DEFAULT '' COMMENT '配置名称',
+  `title` varchar(50) DEFAULT NULL COMMENT '页面标题',
+  `version` int(5) NOT NULL DEFAULT '1',
+  `create_time` datetime NOT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `db` blob,
+  `table` blob,
+  `search` blob,
+  `operate` blob,
+  `storage` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `boot` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `meta_id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `data` longblob NOT NULL,
+  `loaded` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `u` (`meta_id`,`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
